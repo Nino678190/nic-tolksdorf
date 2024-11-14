92b033c4a55551fd152390b2dbff334dff2e83dd
Nino678190 <superwauzi@gmx.de>
2024-11-14 07:11:29 +0100

    Formular-Action aktualisiert, CSS-Anpassungen für Icons und Layout vorgenommen, sowie nicht verwendete Funktionen entfernt

--- script.js
+++ script.js
@@ -10,34 +10,3 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
-
-function contact_me(event){
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const mail = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-    if (name && mail && message){
-        fetch('url', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            }, 
-            body:{
-                'name': name,
-                'email': mail,
-                'message': message
-            }
-        }).then((response) => {
-            if (response.status === 200|| response.status === 201) {
-                alert("Erfolgreich Kontakt aufgenommen")
-            } else {
-                throw new Error("Captcha failed");
-            }
-        }).catch((error) => {
-            console.error('Error:', error);
-            alert("Es ist ein Fehler aufgetreten, bitte versuchen sie es später nochmal");
-        });
-    } else {
-        alert("Bitte alle Felder ausfüllen")
-    }  
-}
