f923dd6d112289bfb39954e7456cfdee3526c599
nino678190 <superwauzi@gmx.de>
2024-11-26 10:59:36 +0100

    Aktualisiere den Kontakt-Link in mehreren HTML-Dateien zu einer Schaltfläche; implementiere ein modales Dialogfeld für soziale Medienlinks in script.js und passe CSS-Variablen an

--- script.js
+++ script.js
@@ -1,12 +1,60 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const textareas = document.querySelectorAll('textarea');
-    textareas.forEach(textarea => {
-        textarea.style.height = 'auto';
-        textarea.style.height = textarea.scrollHeight + 'px';
+document.addEventListener('DOMContentLoaded', function() {
+    const dialog = document.createElement('dialog');
+    dialog.id = 'contact';
+    dialog.innerHTML = `
+        <div class="card">
+            <a class="socialContainer containerOne" href="https://instagram.com/nino.coding" target="_blank">
+                <img src="images/instagram-1-svgrepo-com.svg" class="socialSvg">
+            </a>
+            <a class="socialContainer containerTwo" href="https://wa.me/<number>" target="_blank"> <!--TODO: Anpassen-->
+                <img src="images/WhatsApp.svg" alt="WhatsApp" class="socialSvg">
+            </a>
+            <a class="socialContainer containerThree" href="https://matrix.to/#/@Nic:alpaka.social" target="_blank">
+                <img src="images/element-desktop-logo.svg" alt="Matrix" class="socialSvg">
+            </a>
+            <a class="socialContainer containerFour" href="#" target="_blank"> <!--TODO Anpassen-->
+                <img src="images/mail-open-alt-svgrepo-com.svg" alt="Mail" class="socialSvg">
+            </a>
+        </div>
+    `;
+    document.body.appendChild(dialog);
 
-        textarea.addEventListener('input', function () {
-            textarea.style.height = 'auto';
-            textarea.style.height = textarea.scrollHeight + 'px';
+    const buttons = document.getElementsByClassName('button');
+    const body = document.querySelector('body');
+    
+    Array.from(buttons).forEach(button => {
+        button.addEventListener('click', () => {
+            dialog.showModal();
+            body.style.filter = 'blur(15px)';
         });
     });
+        
+
+    dialog.addEventListener('close', () => {
+        body.style.filter = 'none';
+    });
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape' && dialog.open) {
+            dialog.close();
+        }
+    });
+    dialog.addEventListener('click', () => {
+        dialog.close();
+        body.style.filter = 'none';
+    });
 });
+
+
+
+function fitHeight() {
+    let header = document.querySelector('header');
+    let me = document.getElementById('me');
+    let height = window.innerHeight;
+    let heightHead = header.offsetHeight;
+    let orbit = document.getElementById('orbit');
+    let orbitHeight = orbit.offsetHeight;
+    let newHeight = height - heightHead - 50 + 'px';
+    orbitHeight.style.height = newHeight/100*80 + 'px';
+    me.style.height = newHeight;
+}
