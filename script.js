a3f84443ecb373bd85722338c1a5901992156a9a
Nino678190 <superwauzi@gmx.de>
2025-01-08 20:50:32 +0100

    Aktualisiere den Kontakt-Link in mehreren HTML-Dateien und kommentiere den Dialog im Skript aus

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const dialog = document.createElement('dialog');
-    dialog.id = 'contact';
-    dialog.innerHTML = `
-        <div class="card">
-            <a class="socialContainer containerTwo deactivate" onclick="document.getElementById('contact').close()" target="_blank"> <!--TODO: Anpassen-->
-                <img src="images/WhatsApp.svg" alt="WhatsApp" class="socialSvg">
-            </a>
-            <a class="socialContainer containerThree" href="https://matrix.to/#/@Nic:alpaka.social" target="_blank">
-                <img src="images/element-desktop-logo.svg" alt="Matrix" class="socialSvg">
-            </a>
-            <a class="socialContainer containerFour deactivate" onclick="document.getElementById('contact').close()" target="_blank">
-                <img src="images/mail-open-alt-svgrepo-com.svg" alt="Mail" class="socialSvg">
-            </a>
-        </div>
-    `;
-    document.body.appendChild(dialog);
+// document.addEventListener('DOMContentLoaded', function() {
+//     const dialog = document.createElement('dialog');
+//     dialog.id = 'contact';
+//     dialog.innerHTML = `
+//         <div class="card">
+//             <a class="socialContainer containerTwo deactivate" onclick="document.getElementById('contact').close()" target="_blank"> <!--TODO: Anpassen-->
+//                 <img src="images/WhatsApp.svg" alt="WhatsApp" class="socialSvg">
+//             </a>
+//             <a class="socialContainer containerThree" href="https://matrix.to/#/@Nic:alpaka.social" target="_blank">
+//                 <img src="images/element-desktop-logo.svg" alt="Matrix" class="socialSvg">
+//             </a>
+//             <a class="socialContainer containerFour deactivate" onclick="document.getElementById('contact').close()" target="_blank">
+//                 <img src="images/mail-open-alt-svgrepo-com.svg" alt="Mail" class="socialSvg">
+//             </a>
+//         </div>
+//     `;
+//     document.body.appendChild(dialog);
 
-    const buttons = document.getElementsByClassName('button');
-    const body = document.querySelector('body');
+//     const buttons = document.getElementsByClassName('button');
+//     const body = document.querySelector('body');
     
-    Array.from(buttons).forEach(button => {
-        button.addEventListener('click', () => {
-            dialog.showModal();
-            body.style.filter = 'blur(15px)';
-        });
-    });
+//     Array.from(buttons).forEach(button => {
+//         button.addEventListener('click', () => {
+//             dialog.showModal();
+//             body.style.filter = 'blur(15px)';
+//         });
+//     });
         
 
-    dialog.addEventListener('close', () => {
-        body.style.filter = 'none';
-    });
+//     dialog.addEventListener('close', () => {
+//         body.style.filter = 'none';
+//     });
 
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape' && dialog.open) {
-            dialog.close();
-        }
-    });
-    dialog.addEventListener('click', () => {
-        dialog.close();
-        body.style.filter = 'none';
-    });
-});
+//     document.addEventListener('keydown', (event) => {
+//         if (event.key === 'Escape' && dialog.open) {
+//             dialog.close();
+//         }
+//     });
+//     dialog.addEventListener('click', () => {
+//         dialog.close();
+//         body.style.filter = 'none';
+//     });
+// });
